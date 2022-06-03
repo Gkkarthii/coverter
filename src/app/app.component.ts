3d46f1eeb9351d4ade4dd360b62796f7ab241860
Gkkarthii <105147630+Gkkarthii@users.noreply.github.com>
2022-06-03 14:39:10 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Converter';
+  selected = 'option2';
+  good:any='';
+  display1:any='';
+  display2:any='';
+  getValue(val:string) {
+    this.good=val;
+    // console.warn(val)
+  }
+  conversion(item:string) {
+    if(item==='option1') {
+      const far: { type: any, num: any } = {
+        type: 'C',
+        num: (this.good-32)*1.8
+      };
+      this.display1=far.num;
+      this.display2=far.type;
+      // console.warn(this.display1,this.display2);
+    }
+    else {
+      const cel: { type: any, num:any } = {
+        type: 'F',
+        num: 1.8*this.good+32
+      };
+      this.display1=cel.num;
+      this.display2=cel.type;
+      // console.warn(this.display1,this.display2);
+    }
+  }
+}
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
